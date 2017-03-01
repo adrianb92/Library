acc35fe050696c863ecceaa0ab857f316b624f64
adrianb92 <adrian.barczuk@gmail.com>
2017-03-01 22:07:00 +0100

    Refactoring

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -2,7 +2,7 @@ package app;
 
 public class LibraryApp {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.92";
+        final String appName = "Library v0.93";
         System.out.println(appName);
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -73,4 +73,35 @@ public class LibraryControl {
         for(Option option : Option.values())
             System.out.println(option);
     }
+
+    private enum Option {
+        EXIT(0, "Exit"),
+        ADD_BOOK(1, "Add new book"),
+        ADD_MAGAZINE(2, "Add new magazine"),
+        PRINT_BOOKS(3, "Print all books"),
+        PRINT_MAGAZINES(4, "Print all magazines");
+
+        private int option;
+        private String description;
+
+        Option(int option, String description) {
+            this.option = option;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return option + " - " + description;
+        }
+
+        public static Option createFromInt(int option) throws NoSuchElementException {
+            Option result;
+            try {
+                result = Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                throw new NoSuchElementException("There's no element with such ID!");
+            }
+            return result;
+        }
+    }
 }

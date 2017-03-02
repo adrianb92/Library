022427ff9c3f2f66fa1eb8bc519cf68cf858205e
adrianb92 <adrian.barczuk@gmail.com>
2017-03-02 23:30:55 +0100

    Refactoring

--- src/data/Library.java
+++ src/data/Library.java
@@ -1,9 +1,10 @@
 package data;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class Library implements Serializable{
-    public static final int MAX_PUBLICATIONS = 2000;
+    public static final int MAX_PUBLICATIONS = 1;
     private int publicationsNumber;
     private Publication[] publications;
 
@@ -20,13 +21,27 @@ public class Library implements Serializable{
         publicationsNumber = 0;
     }
 
-    public void addPublication(Publication publication) throws ArrayIndexOutOfBoundsException{
-        if(publicationsNumber == MAX_PUBLICATIONS) {
-            throw new ArrayIndexOutOfBoundsException("Can't add new publication!");
-        }else {
-            publications[publicationsNumber] = publication;
-            publicationsNumber++;
+    public void addPublication(Publication publication){
+        publicationsNumber++;
+        if(publicationsNumber > MAX_PUBLICATIONS) {
+            Arrays.copyOf(publications, publications.length * 2);
         }
+        publications[publicationsNumber-1] = publication;
+    }
+
+    public void removePublication(Publication pub) {
+        if (pub == null)
+            return;
+
+        int i = 0;
+        boolean done = false;
+        do{
+            if(publications[i].equals(pub)){
+                System.arraycopy(publications,i+1,publications,i,publicationsNumber);
+                done = true;
+            }
+            i++;
+        }while(!done);
     }
 
     public void addBook(Book book){
